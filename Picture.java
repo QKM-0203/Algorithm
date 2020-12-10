9d59a2af52dfd5aed49ce3413bf3877d0ae14ec1
QiKaiMeng <QKM-0203>
2020-12-10 22:01:52 +0800

    图

--- Picture.java
+++ Picture.java
@@ -19,34 +19,59 @@ class ArrayPoint{
         next = null;
     }
 }
+class Degree{
+    public int inDegree;
+    public int outDegree;
+    public char  ch;
+
+    public Degree(int inDegree, int outDegree, char ch) {
+        this.inDegree = inDegree;
+        this.outDegree = outDegree;
+        this.ch = ch;
+    }
+}
 public class Main {
-    public static void outSum(Character Char){
-        if(outSum.get(Char) == null){
-            outSum.put(Char,1);
-        }else{
-            outSum.put(Char,outSum.get(Char)+1);
-        }
-
-    }
-    public static void inSum(Character Char){
-        if(inSum.get(Char) == null){
-            inSum.put(Char,1);
-        }else{
-            inSum.put(Char,inSum.get(Char)+1);
+    //计算出度
+    public static void findOutDegree(){
+        for(int i = 0; i < 27; i++){
+            if(ArrPoint[i] != null){
+                PointNode p = ArrPoint[i].next;
+                     while(p != null) {
+                         degree[i].outDegree++;
+                         p = p.next;
+                     }
+            }
         }
     }
-
-    //创建邻接表
+    //创建邻接表，并且邻接表是有序的
     private static void CreatPicture(char char0,char char1){
         PointNode Node = new PointNode(char1 - 65);
         if(ArrPoint[char0 - 65].next == null){
             ArrPoint[char0 - 65].next = Node;
         } else{
             PointNode pre = ArrPoint[char0 - 65].next;
-            while(pre.next != null){
+            PointNode pre1 = pre;
+            if((char)(pre.index+65) > char1){
+                ArrPoint[char0 - 65].next = Node;
+                Node.next = pre;
+                return ;
+            }
+            PointNode p1 = pre;
+            pre = pre.next;
+
+            while(pre!= null){
+                if((char)(pre.index+65) > char1){
+                    p1.next = Node;
+                    Node.next = pre;
+                    return ;
+                }
+                p1 = pre;
                 pre = pre.next;
             }
-            pre.next = Node;
+           while(pre1.next != null){
+               pre1 = pre1.next;
+           }
+           pre1.next = Node;
         }
 
     }
@@ -58,9 +83,9 @@ public class Main {
         }
         //存在邻接边
         while(p != null){
-            if(book[p.index] == 0){
-                book[p.index] = 1;
-                System.out.print((char)(p.index + 65));
+            if(Book[p.index] == 0){
+                Book[p.index] = 1;
+                //System.out.print((char)(p.index + 65));
                 key++;
                 DFS(p.index);
             }
@@ -68,6 +93,7 @@ public class Main {
         }
 
     }
+    //广搜
     private static void BFS(int index){
         PointNode p = ArrPoint[index].next;
         PointNode p1 = null;
@@ -76,57 +102,82 @@ public class Main {
         }
         //存在邻接边
         while(p != null){
-            if(book[p.index] == 0){
-                book[p.index] = 1;
-                System.out.print((char)(p.index + 65));
+            if(Book[p.index] == 0){
+                Book[p.index] = 1;
+                //System.out.print((char)(p.index + 65));
                 key++;
             }
             p1 = p;
             p = p.next;
         }
-        DFS(p1.index);
+        if(p1 != null){
+            DFS(p1.index);
+        }
+        return ;
+
     }
 
-    private static ArrayPoint[] ArrPoint = new ArrayPoint[21];
-    private static HashMap<Character,Integer> outSum = new HashMap<>();
-    private static HashMap<Character,Integer> inSum = new HashMap<>();
-    private static int key = 1;
-    private static int[] book = new int[10];
+    public static ArrayPoint[] ArrPoint = new ArrayPoint[27];
+    public static Degree[] degree = new Degree[27];
+    public static int key = 1;
+    public static int[] Book = new int[27];
+    public static int sumPoint;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int sumPoint = scan.nextInt();
+         sumPoint = scan.nextInt();
         int connectPoint = scan.nextInt();
         String str = scan.next();
         char[] chars = str.toCharArray();
 
         for(int i = 0; i < sumPoint; i++){
-            ArrPoint[i] = new ArrayPoint(chars[i]);
+            ArrPoint[chars[i] - 65] = new ArrayPoint(chars[i]);
+            degree[chars[i] - 65] = new Degree(0,0,chars[i]);
         }
         for (int i = 0; i < connectPoint; i++){
             String PreConnect = scan.next();
             char char0 = PreConnect.charAt(0);
             char char1 = PreConnect.charAt(1);
             CreatPicture(char0,char1);
-            outSum(char0);
-            inSum(char1);
         }
-        Set<Character> outset = outSum.keySet();
-        Iterator<Character> ito = outset.iterator();
-        Set<Character> inset = inSum.keySet();
-        Iterator<Character> iti = inset.iterator();
-        for(Character Char : outset ){
-            System.out.print(Char+" "+outSum.get(Char)+" ");
-            System.out.println(inSum.get(Char));
+       //计算入度
+       for(int i = 0; i < chars.length; i++) {
+           for (int j = 0; j < 27; j++) {
+               if (ArrPoint[j] != null) {
+                   PointNode p = ArrPoint[j].next;
+                   while (p != null) {
+                       if((char)(p.index+65) == chars[i]){
+                           degree[chars[i] - 65].inDegree++;
+                       }
+                       p = p.next;
+                   }
+               }
+           }
+       }
+        findOutDegree();
+        for (int i = 0; i < 27; i++) {
+            if(degree[i] != null){
+                System.out.println(degree[i].ch + " "+degree[i].outDegree + " " + degree[i].inDegree);
+            }
+
         }
         System.out.print("A");
-        book[0] = 1;
+        Book[0] = 1;
         DFS('A' - 65);
         for(int i = 0; i < sumPoint; i++){
-            book[i] = 0;
+            Book[i] = 0;
         }
         key = 1;
         System.out.print("\nA");
         BFS('A' - 65);
+        //求联通分支的个数
+        int sum = 0;
+         for(int i = 0; i < sumPoint; i++){
+             if(Book[chars[i] - 65] == 0 ){
+                 BFS(chars[i] - 65);
+                 sum++;
+             }
+         }
+        System.out.println(sum);
     }
 }
 
